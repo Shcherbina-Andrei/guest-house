63d4a5bbfcb708e5aeb7332d224a3ddacc1e3a00
andrei110295@yandex.ru <andrei110295@yandex.ru>
2022-10-01 21:08:08 +0300

    Сделал сборку

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+import gulp from 'gulp';
+import plumber from 'gulp-plumber';
+import sass from 'gulp-dart-sass';
+import postcss from 'gulp-postcss';
+import csso from 'postcss-csso';
+import autoprefixer from 'autoprefixer';
+import browser from 'browser-sync';
+
+export const styles = () => {
+  return gulp.src('source/sass/style.scss')
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer(),
+      csso()
+    ]))
+    .pipe(gulp.dest('source/css'))
+    .pipe(browser.stream());
+}
+
+const server = (done) => {
+  browser.init({
+    server: {
+      baseDir: 'source'
+    },
+    cors: true,
+    notify: false,
+    ui: false,
+  });
+  done();
+}
+
+const watcher = () => {
+  gulp.watch('source/sass/**/*.sass', gulp.series('styles'));
+  gulp.watch('source/*.html').on('change', browser.reload);
+}
+
+export default gulp.series(
+  styles, server, watcher
+);
